a2fd7824210ab8308c8caa521994c11de1f1a0da
Kien Luyen Trung <luyentru@kiens-mbp.students.clients.local>
2025-01-10 20:40:52 +0100

    Add training func

--- src/pet_fac_rec/train.py
+++ src/pet_fac_rec/train.py
@@ -1,23 +1,50 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
+import random
+import numpy as np
 from pathlib import Path
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from model import MyEfficientNetModel
+from model import MyEfficientNetModel, MyResNet50Model, MyVGG16Model
 from data import MyDataset, get_default_transforms
+from tqdm import tqdm
 
-# Determine the device to use for training
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-print(DEVICE)
+app = typer.Typer()
 
 
+def set_seed(seed_value=42):
+    """Set seed for reproducibility."""
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)  # For CUDA
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def get_model(model_name: str, num_classes: int) -> torch.nn.Module:
+    """
+    Returns the model based on the model name.
+    """
+    if model_name == "efficientnet":
+        return MyEfficientNetModel(num_classes=num_classes)
+    elif model_name == "resnet50":
+        return MyResNet50Model(num_classes=num_classes)
+    elif model_name == "vgg16":
+        return MyVGG16Model(num_classes=num_classes)
+    else:
+        raise ValueError("Unsupported model type provided!")
+
+
+@app.command()
 def train(
+    model_name: str = typer.Option("efficientnet", help="Model type to use ('efficientnet', 'resnet50', 'vgg16')"),
     lr: float = 1e-3,
-    batch_size: int = 32,
-    epochs: int = 1,
+    batch_size: int = 8,
+    epochs: int = 2,
     data_csv: Path = Path("data/data.csv"),
-    num_classes: int = 10,
+    seed: int = 42,
 ) -> None:
     """
     Train the MyEfficientNetModel on the custom dataset.
@@ -29,68 +56,109 @@ def train(
         data_csv (Path): Path to the CSV file containing the preprocessed data.
         num_classes (int): Number of output classes in the dataset.
     """
-    print("Training with custom dataset")
-    print(f"{lr=}, {batch_size=}, {epochs=}, {num_classes=}, {data_csv=}")
+    # Set the seed for reproducibility
+    set_seed(seed)
 
-    # Initialize the model
-    model = MyEfficientNetModel(num_classes=num_classes).to(DEVICE)
+    # Determine the device to use for training
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    print(f"Running on dev: {device}")
+
+    print("Training parameters:")
+    print(f"{lr=}, {batch_size=}, {epochs=}, {data_csv=}")
 
     # Load the dataset
     transform = get_default_transforms()
-    dataset = MyDataset(csv_file=data_csv, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    train_dataset = MyDataset(csv_file=data_csv, split="train", transform=transform)
+    valid_dataset = MyDataset(csv_file=data_csv, split="valid", transform=transform)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+
+    # Initialize the model
+    num_classes = train_dataset.num_classes
+    model = get_model(model_name, num_classes).to(device)
 
     # Define loss function and optimizer
-    loss_fn = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    # Statistics for visualization
-    statistics = {"train_loss": [], "train_accuracy": []}
+    train_losses = []
+    train_accuracies = []
+
+    val_losses = []
+    val_accuracies = []
 
     # Training loop
-    for epoch in range(epochs):
+    epoch_bar = tqdm(range(epochs))
+    for epoch in epoch_bar:
         model.train()
-        for i, (img, target) in enumerate(dataloader):
-            img, target = img.to(DEVICE), target.to(DEVICE)
+        train_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+
+        for img, target in iter(train_dataloader):
+            img, target = img.to(device), target.to(device)
 
             # Forward pass
-            optimizer.zero_grad()
             y_pred = model(img)
-            loss = loss_fn(y_pred, target)
+            loss = criterion(y_pred, target)
 
             # Backward pass and optimization
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            # Record statistics
-            statistics["train_loss"].append(loss.item())
-            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+            train_loss += loss.item() * img.size(0)
+            _, preds = torch.max(y_pred, 1)
+            total_correct += (preds == target).sum().item()
+            total_samples += target.size(0)
 
-            if i % 100 == 0:
-                print(f"Epoch {epoch}, iter {i}, loss: {loss.item():.4f}, accuracy: {accuracy:.4f}")
+        train_losses.append(train_loss / total_samples)
+        train_accuracies.append(total_correct / total_samples)
+
+        val_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+
+        model.eval()
+
+        with torch.no_grad():
+            for img, target in iter(val_dataloader):
+                img, target = img.to(device), target.to(device)
+
+                outputs = model(img)
+                loss = criterion(outputs, target)
+
+                val_loss += loss.item() * img.size(0)
+                _, preds = torch.max(outputs, 1)
+                total_correct += (preds == target).sum().item()
+                total_samples += target.size(0)
+
+        val_losses.append(val_loss / total_samples)
+        val_accuracies.append(total_correct / total_samples)
+        epoch_bar.set_description(
+            f"Epoch {epoch + 1}/{epochs} | Train Loss: {train_losses[-1]:.5f} | Train Acc: {train_accuracies[-1]:.5f} | Val Loss: {val_losses[-1]:.5f} | Val Acc: {val_accuracies[-1]:.5f}"
+        )
 
     print("Training complete")
 
     # Save the model
     torch.save(model.state_dict(), "models/model.pth")
 
+    # TODO: Make a seperate plotting function
     # Plot training statistics
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-    axs[0].plot(statistics["train_loss"])
-    axs[0].set_title("Train loss")
-    axs[1].plot(statistics["train_accuracy"])
-    axs[1].set_title("Train accuracy")
+    fig, axs = plt.subplots(2, 2, figsize=(15, 5))
+    axs[0][0].plot(train_losses)
+    axs[0][0].set_title("Train loss")
+    axs[0][1].plot(train_accuracies)
+    axs[0][1].set_title("Train accuracy")
+    axs[1][0].plot(val_losses)
+    axs[1][0].set_title("Validation loss")
+    axs[1][1].plot(val_accuracies)
+    axs[1][1].set_title("Validation accuracy")
     fig.savefig("reports/figures/training_statistics.png")
 
 
-# Entry point for running the script
-def main() -> None:
-    """
-    Main entry point for the training script.
-    """
-    typer.run(train)
-
-
 if __name__ == "__main__":
-    main()
+    app()
