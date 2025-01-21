2e1cb13f69d7a57df09aea3471a5fe5822858611
Kien Luyen Trung <luyentru@kiens-mbp.students.clients.local>
2025-01-21 16:04:26 +0100

    Refactoring train file

--- src/pet_fac_rec/train.py
+++ src/pet_fac_rec/train.py
@@ -10,7 +10,6 @@ import numpy as np
 import onnx
 import torch
 import typer
-import wandb
 from dotenv import load_dotenv
 from hydra import compose
 from hydra import initialize
@@ -20,6 +19,7 @@ from torch.profiler import record_function
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+import wandb
 from pet_fac_rec.data import MyDataset
 from pet_fac_rec.model import MyEfficientNetModel
 from pet_fac_rec.model import MyResNet50Model
@@ -74,13 +74,114 @@ def get_model(model_name: str, num_classes: int) -> torch.nn.Module:
         raise ValueError("Unsupported model type provided!")
 
 
+def save_and_log_artifact(
+    wandb: wandb,
+    run: wandb.sdk.wandb_run.Run,
+    model_save_path: str,
+    train_losses: List[float],
+    train_accuracies: List[float],
+    val_losses: List[float],
+    val_accuracies: List[float],
+) -> None:
+    """
+    Save and log the trained model as an artifact in Weights & Biases (wandb).
+
+    Args:
+        wandb (wandb): The wandb module.
+        run (wandb.sdk.wandb_run.Run): The current wandb run.
+        model_save_path (str): Path to the saved model file.
+        train_losses (List[float]): List of training losses.
+        train_accuracies (List[float]): List of training accuracies.
+        val_losses (List[float]): List of validation losses.
+        val_accuracies (List[float]): List of validation accuracies.
+    """
+    # Create a new artifact for the model
+    artifact = wandb.Artifact(
+        name=WANDB_COLLECTION,
+        type="model",
+        description="A model trained to classify facial expressions of animals",
+        metadata={
+            "train_loss": train_losses[-1],
+            "train_accuracy": train_accuracies[-1],
+            "valid_loss": val_losses[-1],
+            "valid_accuracy": val_accuracies[-1],
+        },
+    )
+
+    # Add the model file to the artifact
+    artifact.add_file(model_save_path)
+
+    # Log the artifact to the current run
+    logged_art = run.log_artifact(artifact)
+
+    # Link the artifact to a specific path in the wandb registry
+    run.link_artifact(
+        artifact=logged_art,
+        target_path=f"{WANDB_ENTITY_ORG}/{WANDB_REGISTRY}/{WANDB_COLLECTION}",
+        aliases=["staging"],
+    )
+    artifact.save()
+
+
+def save_onnx_file(model_name: str, model: torch.nn.Module, device: torch.device) -> None:
+    """
+    Export a PyTorch model to ONNX format and save it to a file.
+
+    Args:
+        model_name (str): The name of the model.
+        model (torch.nn.Module): The PyTorch model to be exported.
+        device (torch.device): The device on which the model is located.
+    """
+    # Export ONNX file
+    try:
+        # Set the model to evaluation mode
+        model.eval()
+
+        # Create a dummy input tensor with the appropriate shape
+        dummy_input = torch.randn(1, 3, 224, 224).to(device)
+
+        # Define the path to save the ONNX model
+        onnx_save_path = f"models/{model_name}.onnx"
+
+        # Export the model
+        torch.onnx.export(
+            model,
+            dummy_input,
+            onnx_save_path,
+            input_names=["input"],
+            output_names=["output"],
+            dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+            opset_version=11,  # Specify ONNX opset version
+            do_constant_folding=True,  # Optimize constant-folding
+            export_params=True,  # Store the trained parameter weights inside the model file
+        )
+
+        # Verify the model
+        onnx_model = onnx.load(onnx_save_path)
+        onnx.checker.check_model(onnx_model)
+
+        # Add model metadata
+        onnx_model.graph.doc_string = f"Pet facial recognition model using {model_name}"
+        onnx.save(onnx_model, onnx_save_path)
+
+        log.info(f"Model exported and verified to ONNX at {onnx_save_path}")
+    except Exception as e:
+        log.error(f"Failed to export ONNX model: {str(e)}")
+
+
 @app.command()
 def train(
     model_name: str = typer.Option("efficientnet", help="Model type to use ('efficientnet', 'resnet50', 'vgg16')"),
     overrides: Optional[List[str]] = typer.Argument(None),
 ) -> None:
+    """
+    Train a model with the specified configuration.
+
+    Args:
+        model_name (str): The type of model to use ('efficientnet', 'resnet50', 'vgg16').
+        overrides (Optional[List[str]]): A list of configuration overrides.
+    """
     cfg = my_compose(overrides)
-    print(f"Configuration: {OmegaConf.to_yaml(cfg)}")  # Remove later
     log.info(f"Configuration: {OmegaConf.to_yaml(cfg)}")
     hparams = cfg.experiment
 
@@ -91,7 +192,7 @@ def train(
     device = torch.device(
         "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     )
-    print(f"Running on dev: {device}")  # Remove later
+    log.info(f"Running on dev: {device}")  # Remove later
 
     run = wandb.init(
         project=WANDB_PROJECT,
@@ -203,56 +304,9 @@ def train(
     model_save_path = f"models/{model_name}.pth"
     torch.save(model.state_dict(), model_save_path)
     log.info(f"Model saved to {model_save_path}")
-    artifact = wandb.Artifact(
-        name="pet_fac_rec_model",
-        type="model",
-        description="A model trained to classify facial expressions of animals",
-        metadata={
-            "train_loss": train_losses[-1],
-            "train_accuracy": train_accuracies[-1],
-            "valid_loss": val_losses[-1],
-            "valid_accuracy": val_accuracies[-1],
-        },
-    )
-    artifact.add_file(model_save_path)
-    logged_art = run.log_artifact(artifact)
-    run.link_artifact(
-        artifact=logged_art,
-        target_path=f"{WANDB_ENTITY_ORG}/{WANDB_REGISTRY}/{WANDB_COLLECTION}",
-        aliases=["staging"],
-    )
-    artifact.save()
 
-    # Export ONNX file
-    try:
-        model.eval()
-        dummy_input = torch.randn(1, 3, 224, 224).to(device)
-        onnx_save_path = f"models/{model_name}.onnx"
-
-        # Export the model
-        torch.onnx.export(
-            model,
-            dummy_input,
-            onnx_save_path,
-            input_names=["input"],
-            output_names=["output"],
-            dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
-            opset_version=11,  # Specify ONNX opset version
-            do_constant_folding=True,  # Optimize constant-folding
-            export_params=True,  # Store the trained parameter weights inside the model file
-        )
-
-        # Verify the model
-        onnx_model = onnx.load(onnx_save_path)
-        onnx.checker.check_model(onnx_model)
-
-        # Add model metadata
-        onnx_model.graph.doc_string = f"Pet facial recognition model using {model_name}"
-        onnx.save(onnx_model, onnx_save_path)
-
-        log.info(f"Model exported and verified to ONNX at {onnx_save_path}")
-    except Exception as e:
-        log.error(f"Failed to export ONNX model: {str(e)}")
+    save_and_log_artifact(wandb, run, model_save_path, train_losses, train_accuracies, val_losses, val_accuracies)
+    save_onnx_file(model_name, model, device)
 
     # Plot training statistics
     plot_training_statistics(train_losses, train_accuracies, val_losses, val_accuracies)
