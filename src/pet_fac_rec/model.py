b38275600b3fce11ca744f95bcf2d5f847513174
pbc-barho <paulabarho@gmail.com>
2025-01-22 14:23:30 +0100

    Adapted the dummy model to fit the updated model on development

--- src/pet_fac_rec/model.py
+++ src/pet_fac_rec/model.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
-from torchvision.models import efficientnet_b5, resnet50, vgg16
+from torchvision.models import efficientnet_b5
+from torchvision.models import resnet50
+from torchvision.models import vgg16
 
 
 class MyEfficientNetModel(nn.Module):
@@ -8,11 +10,24 @@ class MyEfficientNetModel(nn.Module):
     A model leveraging EfficientNet as the base feature extractor.
     """
 
-    def __init__(self, num_classes: int, pretrained: bool = True) -> None:
+    def __init__(self, num_classes: int, pretrained: bool = True, dropout_rate: float = 0.5) -> None:
         super().__init__()
         self.base_model = efficientnet_b5(pretrained=pretrained)
         num_features = self.base_model.classifier[1].in_features
-        self.base_model.classifier = nn.Sequential(nn.Linear(num_features, num_classes))
+        self.base_model.classifier = nn.Sequential(
+            # 1st layer
+            nn.Linear(num_features, num_features // 2),
+            nn.BatchNorm1d(num_features // 2),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            # 2nd layer
+            nn.Linear(num_features // 2, num_features // 4),
+            nn.BatchNorm1d(num_features // 4),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            # Output layer
+            nn.Linear(num_features // 4, num_classes),
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.base_model(x)
