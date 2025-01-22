de398f1ddfa1afdb0cac017b8c5cfc39b42a96fd
pbc-barho <paulabarho@gmail.com>
2025-01-22 13:13:31 +0100

    Fixed the dummy model to mimic the real model

--- tests/generate_dummy_model.py
+++ tests/generate_dummy_model.py
@@ -4,13 +4,9 @@ from torchvision.models import efficientnet_b5
 
 
 class MyEfficientNetModel(nn.Module):
-    """
-    Mimics the real EfficientNet-based model structure with a dummy feature extractor.
-    """
-
     def __init__(self, num_classes: int, pretrained: bool = False) -> None:
         super().__init__()
-        self.base_model = efficientnet_b5(pretrained=pretrained)
+        self.base_model = efficientnet_b5(weights=None)
         num_features = self.base_model.classifier[1].in_features
         self.base_model.classifier = nn.Sequential(nn.Linear(num_features, num_classes))
 
@@ -18,12 +14,13 @@ class MyEfficientNetModel(nn.Module):
         return self.base_model(x)
 
 
-# Generate a dummy state_dict
 def generate_dummy_model(num_classes: int, model_path: str):
     model = MyEfficientNetModel(num_classes=num_classes)
-    for param in model.parameters():
-        param.data = torch.randn_like(param)
-
+    state_dict = model.state_dict()
+    for key, value in state_dict.items():
+        if value.dtype in (torch.float32, torch.float64):
+            state_dict[key] = torch.randn_like(value)
+    model.load_state_dict(state_dict)
     torch.save(model.state_dict(), model_path)
     print(f"Dummy model saved to {model_path}")
 
